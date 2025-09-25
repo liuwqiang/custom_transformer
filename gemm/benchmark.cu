a925acc6b88851838359854580b3a56d16837380
liuwqiang <liuwqiang>
2025-09-25 21:35:37 +0800

    增加cublas计算

--- gemm/benchmark.cu
+++ gemm/benchmark.cu
@@ -5,6 +5,7 @@
 #include <numeric>
 #include <algorithm>
 #include "gemm.cuh"
+#include <cublas_v2.h>
 
 // Benchmark 测试类
 class Benchmark {
@@ -188,8 +189,87 @@ float matmul_gpu_v2(float* a, float* b, float* out, int M, int N, int K) {
     return milliseconds;
 }
 
+/**
+ * 基于cublas的矩阵运算
+ * cublas的数据布局是以列主序进行存储，而C++中的数组、列表等都是以行主序存储，所以需要对运算值进行一个转置
+ * 转置并不会带来性能上的开销，而是利用 cuBLAS 的列主序解释 + 转置语义，使得内存中的行主序矩阵在数学上被正确地当成行主序矩阵使用
+ * a:[M,K]
+ * b:[K,N]
+ * out:[M,N]
+ */
+float matmul_cublas(const float* a, const float* b, float* out, int M, int N, int K) {
+    //创建handle
+    cublasHandle_t handle;
+    cublasCreate(&handle);
+
+    //在行主序布局下，a 矩阵是 (m, k)，但在列主序下会被解释为 (k, m)。
+    //我们想计算 out = a * b。
+    //通过告诉 cublasSgemv 使用 a 的转置（CUBLAS_OP_T），它就能把内存中按行排布的 a 正确地当作行主序矩阵来处理。
+    //out = alpha * (a @ b) + beta * out
+    float alpha = 1.0f;
+    float beta  = 0.0f;
+
+    //分配显存
+    float* d_a;
+    cudaMalloc(&d_a, M * K * sizeof(float));
+    float* d_b;
+    cudaMalloc(&d_b, K * N * sizeof(float));
+    float* d_out;
+    cudaMalloc(&d_out, M * N * sizeof(float));
+
+    //拷贝数据到显存
+    cudaMemcpy(d_a, a, M * K * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, b, K * N * sizeof(int), cudaMemcpyHostToDevice);
+
+    // ====== 创建 CUDA Events ======
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    // ====== 记录开始时间 ======
+    cudaEventRecord(start, 0);
+
+    cublasStatus_t stat = cublasGemmEx(
+        handle,
+        CUBLAS_OP_T, //a是行主序存储的，需要转置
+        CUBLAS_OP_T, //b也是行主序存储的，也需要转置
+        M, //输出out的行
+        N, //输出out的列
+        K, //a和b的公共维度k
+        &alpha,
+        d_a, CUDA_R_32F, K, //lda K这个我需要解释下，可以简单的理解为我从列主序的存储中获取一行的值需要跨越多少列，这里a的列维度是K
+        d_b, CUDA_R_32F, N, //ldb N这个我需要解释下，可以简单的理解为我从列主序的存储中获取一行的值需要跨越多少列，这里a的列维度是N
+        &beta,
+        d_out, CUDA_R_32F, M,
+        //ldc M这个我更需要解释下了，因为本身计算完out就是列主序存储的，维度是[N,M]，
+        //为了得到正确的输出我们需要让其按照行主序存储[M,N]，那么每得到一个值我们需要跨越M行才能得到正确的位置
+        CUDA_R_32F,
+        CUBLAS_GEMM_DEFAULT
+    );
+
+    // ====== 记录结束时间 ======
+    cudaEventRecord(stop, 0);
+    cudaEventSynchronize(stop);
+
+    float elapsedTime = 0.0f;
+    cudaEventElapsedTime(&elapsedTime, start, stop);
+
+    cudaMemcpy(out, d_out, M * N * sizeof(float), cudaMemcpyDeviceToHost);
+
+    if (stat != CUBLAS_STATUS_SUCCESS) {
+        printf("cublasGemmEx failed: %d\n", stat);
+    }
+    cublasDestroy(handle);
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_out);
+    return elapsedTime;
+}
+
 int main() {
-    unsigned long M = 1024UL, N = 1024UL, K = 768UL, round = 10;
+    int M = 512, N = 512, K = 256, round = 10;
     float* a = (float*) malloc(M * K * sizeof(float));
     rand(a, M * K);
 
@@ -199,7 +279,7 @@ int main() {
     float* out = (float*) malloc(M * N * sizeof(float));
 
     const size_t totalBytes = M * K * sizeof(float) + K * N * sizeof(float) + M * N * sizeof(float);
-    const long flops = 2 * K * M * N;
+    const int flops = 2 * K * M * N;
     Benchmark::run_benchmark(
             round, totalBytes, flops,
             matmul_gpu_v2,
--- gemm/checksum.cu
+++ gemm/checksum.cu
@@ -2,8 +2,8 @@
 #include <cstdlib>
 #include <cmath>
 #include <stdio.h>
-
 #include "gemm.cuh"
+#include <cublas_v2.h>
 
 #define CUDA_CHECK(call) \
 if ((call) != cudaSuccess) { \
@@ -116,8 +116,76 @@ void matmul_gpu_v2(float* a, float* b, float* out, int M, int N, int K) {
     cudaFree(d_out);
 }
 
+/**
+ * 基于cublas的矩阵运算
+ * cublas的数据布局是以列主序进行存储，而C++中的数组、列表等都是以行主序存储，所以需要对运算值进行一个转置
+ * 转置并不会带来性能上的开销，而是利用 cuBLAS 的列主序解释 + 转置语义，使得内存中的行主序矩阵在数学上被正确地当成行主序矩阵使用
+ * a:[M,K]
+ * b:[K,N]
+ * out:[M,N]
+ */
+void matmul_cublas(const float* a, const float* b, float* out, int M, int N, int K) {
+    //创建handle
+    cublasHandle_t handle;
+    cublasCreate(&handle);
+    //分配显存
+    float* d_a;
+    cudaMalloc(&d_a, M * K * sizeof(float));
+    float* d_b;
+    cudaMalloc(&d_b, K * N * sizeof(float));
+    float* d_out;
+    cudaMalloc(&d_out, M * N * sizeof(float));
+
+    cudaMemcpy(d_a, a, M * K * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, b, K * N * sizeof(int), cudaMemcpyHostToDevice);
+
+    //在行主序布局下，a 矩阵是 (m, k)，但在列主序下会被解释为 (k, m)。
+    //我们想计算 out = a * b。
+    //通过告诉 cublasSgemv 使用 a 的转置（CUBLAS_OP_T），它就能把内存中按行排布的 a 正确地当作行主序矩阵来处理。
+    //out = alpha * (a @ b) + beta * out
+    float alpha = 1.0f;
+    float beta  = 0.0f;
+
+    cublasStatus_t stat = cublasGemmEx(
+        handle,
+        CUBLAS_OP_T, //a是行主序存储的，需要转置
+        CUBLAS_OP_T, //b也是行主序存储的，也需要转置
+        M, //输出out的行
+        N, //输出out的列
+        K, //a和b的公共维度k
+        &alpha,
+        d_a, CUDA_R_32F, K, //lda K这个我需要解释下，可以简单的理解为我从列主序的存储中获取一行的值需要跨越多少列，这里a的列维度是K
+        d_b, CUDA_R_32F, N, //ldb N这个我需要解释下，可以简单的理解为我从列主序的存储中获取一行的值需要跨越多少列，这里a的列维度是N
+        &beta,
+        d_out, CUDA_R_32F, M,
+        //ldc M这个我更需要解释下了，因为本身计算完out就是列主序存储的，维度是[N,M]，
+        //为了得到正确的输出我们需要让其按照行主序存储[M,N]，那么每得到一个值我们需要跨越M行才能得到正确的位置
+        CUDA_R_32F,
+        CUBLAS_GEMM_DEFAULT
+    );
+
+    cudaMemcpy(out, d_out, M * N * sizeof(float), cudaMemcpyDeviceToHost);
+
+    if (stat != CUBLAS_STATUS_SUCCESS) {
+        printf("cublasGemmEx failed: %d\n", stat);
+    }
+    cublasDestroy(handle);
+
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_out);
+}
+
+void transpose_cpu(const float* in, float* out, int rows, int cols) {
+    for (int r = 0; r < rows; ++r) {
+        for (int c = 0; c < cols; ++c) {
+            out[c * rows + r] = in[r * cols + c];
+        }
+    }
+}
+
 int main(int argc, char *argv[]) {
-    int M = 1024, N = 1024, K = 1024;
+    int M = 512, N = 512, K = 256;
     float* a = (float*) malloc(M * K * sizeof(float));
     rand(a, M * K);
 
@@ -128,9 +196,12 @@ int main(int argc, char *argv[]) {
     float* d_out = (float*) malloc(M * N * sizeof(float));
 
     matmul_cpu(a, b, out, M, N, K);
-    matmul_gpu_v2(a, b , d_out, M, N, K);
+    matmul_cublas(a, b , d_out, M, N, K);
 
-    if (check(out, d_out, M * N)) {
+    float* final_out = (float*) malloc(M * N * sizeof(float));
+    transpose_cpu(d_out, final_out, M, N);
+
+    if (check(out, final_out, M * N)) {
         printf("the ans is right\n");
     } else {
         printf("the ans is wrong\n");
